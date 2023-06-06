bbd53e3fbfa72b8948ee9f3f1a8ee3a8d4afe189
Juma852 <camithe0016@outlook.es>
2023-06-05 20:07:06 -0500

    nw rls

---
+++ app.js
@@ -0,0 +1,9 @@
+const $btnResponsive = document.getElementById("btn")
+
+const $listNavBar = document.getElementById("ul-navbar");
+
+$btnResponsive.addEventListener("click",()=>{
+
+$listNavBar.classList.toggle("toggle-activate");
+
+})
